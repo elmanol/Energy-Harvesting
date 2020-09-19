cda2fd75e7853e1c7f701474e62b68f2dfe873ce
Manolis Kerimakis <kerimakis@ceid.upatras.gr>
2020-09-19 20:35:36 +0300

    new_parameters

--- main.m
+++ main.m
@@ -6,17 +6,17 @@
 parameters;
 
 %create empty sensors vector
-sensors = sensor_node.empty;
+node = sensor_node.empty;
 
 %create a sensor_node type object for each sensor
 for i=1:number_of_sensors
-    sensors(i) = sensor_node(initial_battery, x(i), y(i), transmission_range); 
+    node(i) = sensor_node(initial_battery, x, y, i, transmission_range); 
 end
-decrease_battery(sensors(i), 1);
+%decrease_battery(node(i), 1);
 
-%figure of the sensors positions
-f1 = figure;
-plot(x,y,"*");
+%figure of the nodes positions
+%f1 = figure;
+%plot(x,y,"*");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %main loop
